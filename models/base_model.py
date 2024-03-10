118b5200abb5eb00cd1c904ab8429de779b6c193
bozize <abdiazizibrahim56@.com>
2024-03-10 05:32:33 -0700

    updated

--- models/base_model.py
+++ models/base_model.py
@@ -14,11 +14,19 @@ class BaseModel:
     updated_at; the time the model is updated.
 
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         initializes the basemodel.
 
         """
+        if kwargs:
+            # iterste over the key word #
+            for key, value in kwargs.items():
+                if key != (__class__):
+                    if key ["created_at", "updated_at"]:
+                        value = datetime.fromisoformat(value)
+                        setattr(self, key, value)
+        else:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
