226b33a7de649876f4d5f0cf39cbba0e5791905d
bozize <abdiazizibrahim56@.com>
2024-03-10 06:39:07 -0700

    updated

--- models/base_model.py
+++ models/base_model.py
@@ -19,16 +19,17 @@ class BaseModel:
         initializes the basemodel.
 
         """
-        #if kwargs:
+        if kwargs:
             # iterste over the key word #
-            #for key, value in kwargs.items():#
+            for key, value in kwargs.items():
                 #if key != "__class__":#
-                    #if key in ["created_at", "updated_at"]:#
-                        #value = datetime.fromisoformat(value)#
-                        #setattr(self, key, value)#  
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+                    if key in ["created_at", "updated_at"]:
+                        value = datetime.fromisoformat(value)
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
