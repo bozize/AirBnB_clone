13b703e79f29d77f1f49790bbf4ac2b5c1201250
bozize <abdiazizibrahim56@.com>
2024-03-10 20:11:16 -0700

    ip

--- models/__init__.py
+++ models/__init__.py
@@ -5,6 +5,5 @@ This used to create unique instance of file storage.
 
 from models.engine.file_storage import FileStorage
 
-
 storage = FileStorage()
 storage.reload()
--- models/base_model.py
+++ models/base_model.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 from datetime import datetime
 import uuid
-import models import storage
-
 
 class BaseModel:
     """
