71ac0824434c4e3b6b7653d4a3fe9deed909c647
bozize <abdiazizibrahim56@.com>
2024-03-12 13:53:08 -0700

    up

--- models/base_model.py
+++ models/base_model.py
@@ -20,19 +20,19 @@ class BaseModel:
         initializes the basemodel.
 
         """
+        dtt = "%Y-%m-%dT%H:%M:%S.%f"
         if kwargs:
             # iterste over the key word #
             for key, value in kwargs.items():
                 if key != "__class__":
                     if key in ["created_at", "updated_at"]:
-                        setattr(self, key, datetime.fromisoformat(value))
+                        setattr(self, key, datetime.strptime(value, dtt))
                     else:
                         setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
 
     def __str__(self):
         """
